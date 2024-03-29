fbe5f124dad118d05600da283a2825801b48d868
de-mawo <groovebassist1@gmail.com>
2024-03-29 17:49:04 +0200

    [Feature]: Added get routes for the app

--- api/index.js
+++ api/index.js
@@ -1,13 +1,51 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.get("/user", (req, res) => {
+  const user = [
+    {
+      id: "1",
+      name: "Prince",
+      email: "info@bath.co.za",
+    },
+    {
+      id: "1",
+      name: "Prince",
+      email: "info@bath.co.za",
+    },
+    {
+      id: "1",
+      name: "Prince",
+      email: "info@bath.co.za",
+    },
+    {
+      id: "1",
+      name: "Prince",
+      email: "info@bath.co.za",
+    },
+  ];
+  res.json(user).status(200);
+});
+
+app.post('/users', (req, res) => {
+  const data = req.body;
+  console.log(data);
+  res.json("success").status(200)
+
 })
+
+app.delete("/users/:id")
+
+
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
 
 module.exports = app;
